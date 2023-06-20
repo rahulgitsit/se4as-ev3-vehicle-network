d8ff0241af9b42c5079f0f03d49f15443c4006f1
Aswin P Jagadeesh <aswinpjagadeesh@gmail.com>
2023-06-20 12:32:52 +0200

    Update Knowledge.py

--- src/ev3_pc_server_normal/MAPE/Knowledge.py
+++ src/ev3_pc_server_normal/MAPE/Knowledge.py
@@ -1,3 +1,4 @@
+import csv
 class Knowledge:
     def __init__(self):
         self.perv_cmd=0
@@ -7,3 +8,19 @@ class Knowledge:
         self.emerg=0
         self.ack=200
         self.emergover=5
+        self.columnname1=["Time","First vehicle lane","First vehicle speed","First vehicle parking","First Vehicle Obstacle distance","Color from first vehicle","Reflection from first vehicle","RGB from first vehicle"]
+        self.columnname2=["Time","second vehicle speed","second vehicle lane","second vehicle parking","second Vehicle Obstacle distance","Color from second vehicle","Reflection from second vehicle","RGB from second vehicle"]
+        with open('normal1av.csv', 'w', encoding='UTF8',newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(self.columnname1)
+        with open('normalav2av.csv', 'w', encoding='UTF8',newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(self.columnname2)
+    def write_normalav1_log(self,var_list:list):
+        with open('normal1av.csv', 'a', newline='') as p:
+                writer = csv.writer(p)
+                writer.writerow(var_list)
+    def write_normalav2_log(self,var_list:list):
+        with open('normalav2av.csv', 'a', newline='') as p:
+                writer = csv.writer(p)
+                writer.writerow(var_list)
